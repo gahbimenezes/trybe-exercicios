6a1cb58aa8e540cd3082aa131291918b6516cd53
Gabriela Menezes <gahbi.menezes@gmail.com>
2022-02-09 22:54:43 -0300

    modulo1/bloco5/dia1 - realizaçao dos exercicios

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicio1/script.js
@@ -0,0 +1,23 @@
+let cabecalho = document.getElementById("header-container");
+cabecalho.style.backgroundColor = "#01b069";
+
+let emergency = document.querySelectorAll(".emergency-tasks")[0];
+emergency.style.backgroundColor = "salmon";
+
+let emergency2 = document.querySelectorAll(".emergency-tasks h3")[0];
+emergency2.style.backgroundColor = "#a534f1";
+
+let emergency3 = document.querySelectorAll(".emergency-tasks h3")[1];
+emergency3.style.backgroundColor = "#a534f1";
+
+let noEmergency = document.querySelectorAll(".no-emergency-tasks")[0];
+noEmergency.style.backgroundColor = "yellow";
+
+let noEmergency2 = document.querySelectorAll(".no-emergency-tasks h3")[0];
+noEmergency2.style.backgroundColor = "black";
+
+let noEmergency3 = document.querySelectorAll(".no-emergency-tasks h3")[1];
+noEmergency3.style.backgroundColor = "black";
+
+let rodape = document.getElementById("footer-container");
+rodape.style.backgroundColor = "#053937";
