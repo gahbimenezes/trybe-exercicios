ab0e82cb61d3062d1b71a4d766e002fc2df0d422
Gabriela Menezes <gahbi.menezes@gmail.com>
2022-03-10 23:56:36 -0300

    atualização módulo1/bloco8/dia3 - adição do exercício 5

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-hof-map-e-filter/exercise5.js
@@ -0,0 +1,93 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+  // Adicione o código do exercício aqui:
+
+  const expectedResult = [
+    'Frank Herbert',
+    'George R. R. Martin',
+    'Isaac Asimov',
+    'J. R. R. Tolkien',
+  ];
+  
+  function fantasyOrScienceFictionAuthors(array) {
+    // escreva seu código aqui
+        const obj = array.filter((item) => item.genre === 'Fantasia' || item.genre === 'Ficção Científica');
+
+        const authors = obj.map((item2) => item2.author.name)
+
+        authors.sort(function (a, b) {
+            if (a > b) {
+            return 1;
+            }
+            if (a < b) {
+            return -1;
+            }
+            // a must be equal to b
+            return 0;
+        });
+
+      return authors;
+    }
+
+  console.log(fantasyOrScienceFictionAuthors(books));
