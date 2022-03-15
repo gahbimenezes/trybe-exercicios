9c1ccc1c399f39e108ce0ec9f56851a13380ed43
Gabriela Menezes <gahbi.menezes@gmail.com>
2022-03-15 00:27:21 -0300

    atualização do módulo1/bloco8/dia4 - adição dos exercícios 5 e 6

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-hof-reduce/exercícios/exercise5.js
@@ -0,0 +1,22 @@
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+    
+];
+  
+function containsA(array) {
+    // escreva seu código aqui
+
+    //GABARITO
+
+    // return array.reduce((acc, curr) =>
+    // acc + curr.split('').reduce((acumulator, current) => {
+    //    if (current === 'a' || current === 'A') return acumulator + 1;
+    //    return acumulator;
+    // }, 0), 0);
+     
+       
+};
+
+console.log(containsA(names));
